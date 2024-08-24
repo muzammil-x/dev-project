e7b0a16bdbebdc454ef4fd48a3630d4c3fb6903a
muzammil-x <m.muzammilx2@gmail.com>
2024-08-24 14:43:46 +0500

    banner set

--- portfolio/src/components/Banner.jsx
+++ portfolio/src/components/Banner.jsx
@@ -6,7 +6,7 @@ import { FaYoutube } from "react-icons/fa";
 function Banner() {
   return (
     <>
-      <section className="bg-[#25262F] w-full pt-40 pb-32 ">
+      <section className="bg-[#25262F] w-full pt-40 pb-[90px] md:pb-[123px]   ">
         <div className="flex flex-col justify-center items-center relative lg:flex-row lg:justify-around">
           <div className="flex flex-col justify-center items-center mt-9  lg:items-start  xl:ml-5">
             <div className="text-[18px] tracking-[4px] text-[#80DB66] lg:text-3xl ">
@@ -30,7 +30,7 @@ function Banner() {
               </button>
             </div>
           </div>
-          <div className="mt-[120px] lg:mt-[100px] h-64 w-80 lg:w-[30rem] lg:h-[10rem] flex justify-center items-center">
+          <div className="mt-[120px] lg:mt-[100px] h-64 w-80 lg:w-[30rem] lg:h-[10rem] md:pt-16 lg:pt-0 flex justify-center items-center">
             <img src="/person.png" alt="" className="mt-4" />
           </div>
           <div className="hidden  md:block absolute top-[120px] right-6  text-white w-16  space-y-3">
