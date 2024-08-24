58540f09dd5606f3c32966eb9de892921e617f45
muzammil-x <m.muzammilx2@gmail.com>
2024-08-24 15:15:01 +0500

    card fix

--- portfolio/src/components/Offer.jsx
+++ portfolio/src/components/Offer.jsx
@@ -1,61 +1,69 @@
 function Offer() {
   return (
     <>
-      <div className="main bg-[#25262F] flex flex-col items-center justify-center p-16 w-auto">
-        <div>
-          <h1 className="font-semibold text-xl text-[#80DB66]">SERVICES</h1>
+      <div className="main bg-[#25262F] flex flex-col items-center justify-center p-8 lg:p-16 w-auto">
+        <div className="text-center">
+          <h1 className="font-semibold text-lg lg:text-xl text-[#80DB66]">
+            SERVICES
+          </h1>
         </div>
-        <div>
-          <h3 className="text-white text-5xl font-semibold">What I Offer</h3>
+        <div className="text-center mt-2">
+          <h3 className="text-white text-3xl lg:text-5xl font-semibold">
+            What I Offer
+          </h3>
         </div>
-        <div className="sm:m-16 boxes grid grid-cols-2 gap-5 py-5">
-          <div className=" bg-white flex flex-row gap-5 items-center rounded-xl px-10 py-10">
+        <div className="sm:mt-8 grid grid-cols-1 sm:grid-cols-2 gap-5 py-5">
+          <div className="bg-white flex flex-col sm:flex-row gap-5 items-center rounded-xl px-5 py-5 lg:px-10 lg:py-10">
             <div>
-              <img src="/o2.png" alt="" />
+              <img src="/o2.png" alt="Web Development" />
             </div>
-
             <div>
-              <h1 className="font-semibold text-xl">Web Development</h1>
-              <p className=" text-md">
+              <h1 className="font-semibold text-lg lg:text-xl">
+                Web Development
+              </h1>
+              <p className="text-sm lg:text-md">
                 Ludantium totam rem aperia meaququae ab tatis et quasi
                 architecto beatae vit dunt ut labore et dolore magna aliqua.
               </p>
             </div>
           </div>
-          <div className=" bg-white flex flex-row gap-5 items-center rounded-xl px-10 py-10">
+          <div className="bg-white flex flex-col sm:flex-row gap-5 items-center rounded-xl px-5 py-5 lg:px-10 lg:py-10">
             <div>
-              <img src="/o3.png" alt="" />
+              <img src="/o3.png" alt="Software Development" />
             </div>
-
             <div>
-              <h1 className="font-semibold text-xl">Software Development</h1>
-              <p className=" text-md">
+              <h1 className="font-semibold text-lg lg:text-xl">
+                Software Development
+              </h1>
+              <p className="text-sm lg:text-md">
                 Ludantium totam rem aperia meaququae ab tatis et quasi
                 architecto beatae vit dunt ut labore et dolore magna aliqua.
               </p>
             </div>
           </div>
-          <div className=" bg-white flex flex-row gap-5 items-center rounded-xl px-10 py-10">
+          <div className="bg-white flex flex-col sm:flex-row gap-5 items-center rounded-xl px-5 py-5 lg:px-10 lg:py-10">
             <div>
-              <img src="/o4.png" alt="" />
+              <img src="/o4.png" alt="System Development" />
             </div>
-
             <div>
-              <h1 className="font-semibold text-xl">System Development</h1>
-              <p className=" text-md">
+              <h1 className="font-semibold text-lg lg:text-xl">
+                System Development
+              </h1>
+              <p className="text-sm lg:text-md">
                 Ludantium totam rem aperia meaququae ab tatis et quasi
                 architecto beatae vit dunt ut labore et dolore magna aliqua.
               </p>
             </div>
           </div>
-          <div className=" bg-white flex flex-row gap-5 items-center rounded-xl px-10 py-10">
+          <div className="bg-white flex flex-col sm:flex-row gap-5 items-center rounded-xl px-5 py-5 lg:px-10 lg:py-10">
             <div>
-              <img src="/o1.png" alt="" />
+              <img src="/o1.png" alt="Security Analysis" />
             </div>
-
             <div>
-              <h1 className="font-semibold text-xl">Security Analysis</h1>
-              <p className=" text-md">
+              <h1 className="font-semibold text-lg lg:text-xl">
+                Security Analysis
+              </h1>
+              <p className="text-sm lg:text-md">
                 Ludantium totam rem aperia meaququae ab tatis et quasi
                 architecto beatae vit dunt ut labore et dolore magna aliqua.
               </p>
@@ -66,4 +74,5 @@ function Offer() {
     </>
   );
 }
+
 export default Offer;
